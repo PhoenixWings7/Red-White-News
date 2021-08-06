ed3267897c1050ed949c9532c3a80b97ba5dc869
PhoenixWings7 <adamus.klaudia.pasp@gmail.com>
2021-08-06 19:27:08 +0200

    fix a method signature bug

--- app/src/main/java/com/phoenixwings7/white_red_news/MainActivity.java
+++ app/src/main/java/com/phoenixwings7/white_red_news/MainActivity.java
@@ -1,15 +1,13 @@
 package com.phoenixwings7.white_red_news;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements NewsFeedContract.View {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
